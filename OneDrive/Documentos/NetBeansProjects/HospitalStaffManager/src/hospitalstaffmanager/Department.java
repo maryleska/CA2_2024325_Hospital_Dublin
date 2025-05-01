d50c0dbec7295ff6ad7d4868e683706d9f997d37
maryleska <2024325@student.cct.ie>
2025-05-01 18:37:58 +0100

    Department code first

---
+++ OneDrive/Documentos/NetBeansProjects/HospitalStaffManager/src/hospitalstaffmanager/Department.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package hospitalstaffmanager;
+
+/**
+ *
+ * @author maryl
+ */
+public enum Department {
+    EMERGENCY,
+    SURGERY,
+    PEDIATRICS;
+}
